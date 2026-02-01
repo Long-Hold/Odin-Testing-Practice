bc46b5e910c27f30026e5a917ebeefe778136a7c
Long-Hold <102175343+Long-Hold@users.noreply.github.com>
2026-02-01 01:56:36 -0500

    feat: Add divide method and respective tests

--- src/modules/functions/calculator.js
+++ src/modules/functions/calculator.js
@@ -2,5 +2,11 @@ export function createCalculator() {
     return {
         add: (num1, num2) => { return num1 + num2; },
         subtract: (num1, num2) => { return num1 - num2 ;},
+        divide: (num1, num2) => {
+            if (num2 === 0) {
+                throw new RangeError('cannot divide by 0.');
+            } 
+            return num1 / num2 
+        },
     }
 }
--- src/modules/tests/calculator.test.js
+++ src/modules/tests/calculator.test.js
@@ -21,5 +21,17 @@ describe('createCalculator', () => {
         ])('subtracts "%i" - "%i" and returns "%i".', (num1, num2, result) => {
            expect(calculator.subtract(num1, num2)).toEqual(result); 
         });
+    });
+    describe('divide()', () => {
+        test.each([
+            [2, 2, 1],
+            [3, 2, 1.5],
+            [-2, 4, -0.5],
+        ])('divides "%i" by "%i" and returns "%f".', (num1, num2, result) => {
+            expect(calculator.divide(num1, num2)).toEqual(result);
+        });
+        test('throws RangeError when num2 is 0', () => {
+            expect(() => calculator.divide(1, 0)).toThrow(RangeError);
+        })
     })
 });
