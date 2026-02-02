704ef2e8a06ca23b39ebe6afd6383ee7823e526c
Matthew <102175343+Long-Hold@users.noreply.github.com>
2026-02-02 08:29:10 -0500

    feat: Add extra tests

--- src/modules/tests/analyzeArray.test.js
+++ src/modules/tests/analyzeArray.test.js
@@ -7,6 +7,21 @@ describe('analyzeArray()', () => {
             expected: { average: 4, min: 1, max: 8, length: 6 },
             description: 'positive integers',
         },
+        {
+        input: [5],
+        expected: { average: 5, min: 5, max: 5, length: 1 },
+        description: 'single element'
+        },
+        {
+        input: [-1, -5, -3],
+        expected: { average: -3, min: -5, max: -1, length: 3 },
+        description: 'negative numbers'
+        },
+        {
+        input: [1.5, 2.5, 3.5],
+        expected: { average: 2.5, min: 1.5, max: 3.5, length: 3 },
+        description: 'decimals'
+        }
     ])('analyzes an array with $description', ({input, expected}) => {
         expect(analyzeArray(input)).toStrictEqual(expected);
     });
