f289aae8f51e3701441686204f1f30feeb74712e
Matthew <102175343+Long-Hold@users.noreply.github.com>
2026-02-02 08:20:34 -0500

    feat: Write first test for analyze func

---
+++ src/modules/tests/analyzeArray.test.js
@@ -0,0 +1,13 @@
+import { analyzeArray } from "../functions/analyzeArray";
+
+describe('analyzeArray()', () => {
+    test.each([
+        {
+            input: [1,8,3,4,2,6],
+            expected: { average: 4, min: 1, max: 8, length: 6 },
+            description: 'positive integers',
+        },
+    ])('analyzes an array with $description', ({input, expected}) => {
+        expect(analyzeArray(input)).toBe(expected);
+    });
+});
