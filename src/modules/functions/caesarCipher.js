22a909968d3d54d9f3c11deb261e8b8dfd8fc5cc
Matthew <102175343+Long-Hold@users.noreply.github.com>
2026-02-02 01:26:36 -0500

    feat: Test if char is alphabetical before shifting

--- src/modules/functions/caesarCipher.js
+++ src/modules/functions/caesarCipher.js
@@ -1,3 +1,18 @@
 export function caesarCipher(word, shiftFactor) {
+    const cipherCodes = [];
+    for (const char of word) {
+        if (!isAlphabetical(char)) {
+            cipherCodes.push(char.charCodeAt());
+            continue;
+        }
 
+        cipherCodes.push(char.charCodeAt() + shiftFactor);
+    }
+
+    return String.fromCharCode(...cipherCodes);
 }
+
+function isAlphabetical(char) {
+    return /[a-zA-Z]/.test(char);
+}
+
